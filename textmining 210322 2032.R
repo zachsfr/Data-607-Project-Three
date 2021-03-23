ffc84a97692caf750e8a2413190c8e7ae7d97b1e
Daniel Moscoe <dmoscoe@gmail.com>
2021-03-23 10:35:39 -0400

    R code now fully functional!

--- textmining 210322 2032.R
+++ textmining 210322 2032.R
@@ -1,17 +1,15 @@
 #This script takes atlanta, a folder of txt job descriptions for data scientists. It generates ds_skills_df, a dataframe. ds_skills_df has 1 row for each job listing in atlanta, and one column for each term in ds_skills_list. The value in each cell is the number of appearances of the column name in the listing.
 
-#Current issues: I can't detect occurrences of C#, C++, or R.
-
 library(tidyverse)
 library(tm)
 
 atlanta <- "C:/Users/dmosc/OneDrive/Documents/academic/CUNY SPS/DATA 607/Proj3/zachsfr project three/Data-607-Project-Three/atlanta"
 
-find <- c("computer science","data engineering","deep learning","machine learning","neural networks","project management","scikit-learn","software development","software engineering")
+find <- c("[^[[:alnum:]][Cc]\\#","[^[[:alnum:]][Cc]\\+\\+","computer science","data engineering","deep learning","machine learning","neural networks","project management","[^[[:alnum:]][Rr][^[[:alnum:]]","scikit-learn","software development","software engineering")
 
-repl <- c("computerscience","dataengineering","deeplearning","machinelearning","neuralnetworks","projectmanagement","scikitlearn","softwaredevelopment","softwareengineering")
+repl <- c(" csharp"," cplusplus","computerscience","dataengineering","deeplearning","machinelearning","neuralnetworks","projectmanagement"," rrrr","scikitlearn","softwaredevelopment","softwareengineering")
 
-ds_skills_list <- c("ai","analysis","aws","azure","c","caffe","cassandra","communication","computerscience","cplusplus","csharp","d3","dataengineering","deeplearning","docker","excel","git","hadoop","hbase","hive","java","javascript","keras","linux","machinelearning","mathematics","matlab","mongodb","mysql","neuralnetworks","nlp","nosql","numpy","pandas","perl","pig","projectmanagement","python","pytorch","sas","scala","scikitlearn","softwaredevelopment","softwareengineering","spark","spss","sql","statistics","tableau","tensorflow","visualization")
+ds_skills_list <- c("ai","analysis","aws","azure","c","caffe","cassandra","communication","computerscience","cplusplus","csharp","d3","dataengineering","deeplearning","docker","excel","git","hadoop","hbase","hive","java","javascript","keras","linux","machinelearning","mathematics","matlab","mongodb","mysql","neuralnetworks","nlp","nosql","numpy","pandas","perl","pig","projectmanagement","python","pytorch","rrrr","sas","scala","scikitlearn","softwaredevelopment","softwareengineering","spark","spss","sql","statistics","tableau","tensorflow","visualization")
 
 #Create corpus from Atlanta files#
 
@@ -22,11 +20,13 @@ atlanta_corpus <- VCorpus(DirSource(atlanta, encoding = "UTF-8"), readerControl 
 atlanta_corpus <- tm_map(atlanta_corpus, removeWords, stopwords("english"))
 atlanta_corpus <- tm_map(atlanta_corpus, stripWhitespace)
 atlanta_corpus <- tm_map(atlanta_corpus, content_transformer(tolower))
-atlanta_corpus <- tm_map(atlanta_corpus, removePunctuation)
+#atlanta_corpus <- tm_map(atlanta_corpus, removePunctuation) so I can detect C#, C++
 
 for (i in seq(length(find))) {
   atlanta_corpus <- tm_map(atlanta_corpus, content_transformer(function(atlanta_corpus) gsub(atlanta_corpus, pattern = find[i], replacement = repl[i])))
 }
+
+atlanta_corpus <- tm_map(atlanta_corpus, removePunctuation) ###########
 
 #build document_term dataframe#
 
